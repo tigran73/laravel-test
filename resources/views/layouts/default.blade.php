5e98215ab7722c1f08aab7838ddce200e901a8c1
Tigran Sarkisyan <tigran.sarkisyan96@gmail.com>
2025-03-11 18:02:34 +0500

    fix: добавил базу в докере, и дефолтный шаблон + bs5

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @yield('meta_desc')
+
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body>
+<header>
+@include('header')
+</header>
+
+<main>
+@yield('content')
+</main>
+
+<footer>
+@include('footer')
+</footer>
+
+<script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+</html>
